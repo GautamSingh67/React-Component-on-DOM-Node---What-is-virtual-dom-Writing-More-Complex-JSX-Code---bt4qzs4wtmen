ac70458e8d0371bb2c7243d282bd39afd4c8f452
Gautam Singh <113767284+GautamSingh67@users.noreply.github.com>
2022-12-05 12:42:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import React from "React";
-const App = ()=>{
-return(
-<div>
-  
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-  </div>
-)
+import React from "react";
+
+const App = () => {
+return (
+    <div>
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  )
 }
+
 export default App;
-
-
-
-
-
-
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
